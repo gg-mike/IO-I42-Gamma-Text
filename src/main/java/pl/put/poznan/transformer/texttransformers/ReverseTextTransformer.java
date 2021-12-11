c7884425ea2a3fb8a74b9da27f97dde751544b8e
Michael Grygiel <47571348+gg-mike@users.noreply.github.com>
2021-12-11 11:53:04 +0100

    Update ReverseTextTransformer.java

--- src/main/java/pl/put/poznan/transformer/texttransformers/ReverseTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/texttransformers/ReverseTextTransformer.java
@@ -1,5 +1,8 @@
 package pl.put.poznan.transformer.texttransformers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
 /**
  * Reverse String but preserve the capitalization of letters (indexes of capitalized letters don't change)
  *
