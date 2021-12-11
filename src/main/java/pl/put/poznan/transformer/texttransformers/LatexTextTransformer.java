c09d3d0bbda7e76d4322f27dc8ca727c91bac7bb
Michael Grygiel <47571348+gg-mike@users.noreply.github.com>
2021-12-11 18:29:00 +0100

    Latex Text Transformer (#9)
    * Add ReverseTextTransformer
    
    * Add LatexTextTransformer

---
+++ src/main/java/pl/put/poznan/transformer/texttransformers/LatexTextTransformer.java
@@ -0,0 +1,55 @@
+package pl.put.poznan.transformer.texttransformers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+/**
+ * Changes characters to be compatible with Latex
+ *
+ * @author Michał Grygiel
+ * @see TextTransformer
+ */
+@Slf4j
+@Component
+public class LatexTextTransformer implements TextTransformer {
+    private final Map<Character, String> specialSymbols = Map.ofEntries(
+            entry('%', "\\%"),
+            entry('$', "\\$"),
+            entry('{', "\\{"),
+            entry('}', "\\}"),
+            entry('_', "\\_"),
+            entry('¶', "\\P"),
+            entry('|', "\\textbar"),
+            entry('>', "\\textgreater"),
+            entry('<', "\\textless"),
+            entry('-', "\\textdash"),
+            entry('™', "\\testtrademark"),
+            entry('¡', "\\texteclamdown"),
+            entry('¿', "\\textquestiondown"),
+            entry('£', "\\pounds"),
+            entry('#', "\\#"),
+            entry('&', "\\&"),
+            entry('§', "\\S"),
+            entry('\\', "\\textbackslash"),
+            entry('—', "\\textemdash"),
+            entry('®', "\\textregistered"),
+            entry('ⓐ', "\\textcircled{a}"),
+            entry('©', "\\copyright")
+    );
+
+    @Override
+    public String transform(String text) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            if (specialSymbols.containsKey(text.charAt(i)))
+                builder.append(specialSymbols.get(text.charAt(i)));
+            else
+                builder.append(text.charAt(i));
+        }
+        return builder.toString();
+    }
+}
