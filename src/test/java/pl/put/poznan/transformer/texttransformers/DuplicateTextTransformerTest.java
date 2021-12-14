2b9787c3ddaa7233fed1f29d86d6fc8c898ae329
gg-mike <michal.g.grygiel@gmail.com>
2021-12-14 13:32:50 +0100

    Add DuplicateTextTransformer

---
+++ src/test/java/pl/put/poznan/transformer/texttransformers/DuplicateTextTransformerTest.java
@@ -0,0 +1,34 @@
+package pl.put.poznan.transformer.texttransformers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DuplicateTextTransformerTest {
+    private DuplicateTextTransformer transformer;
+
+    @BeforeEach
+    void init() { transformer = new DuplicateTextTransformer(); }
+
+    @Test
+    void testSimpleTransformation() {
+        assertEquals("aaa bb c ddd ee", transformer.transform("aaa bb c c c c ddd ee ee"));
+    }
+
+    @Test
+    void testDifferentCaseTransformation() {
+        assertEquals("aaa bb c ddd eE", transformer.transform("aaa bb c C C c ddd eE Ee"));
+    }
+
+    @Test
+    void testDifferentSeparatorTransformation() {
+        assertEquals("aaa bb c ddd ee", transformer.transform("aaa \tbb c c    c  c ddd ee\t ee"));
+    }
+
+    @Test
+    void testDifferentCaseAndSeparatorTransformation() {
+        assertEquals("aaa bb C ddd eE", transformer.transform("aaa \tbb C c    C  c ddd eE\t Ee"));
+    }
+
+}
