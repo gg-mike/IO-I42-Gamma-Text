734300aaad7a4d47c7f7df69c15502d869d0a4e4
KanarXD <piciolo@outlook.com>
2021-12-17 19:09:00 +0100

    changes

--- src/test/java/pl/put/poznan/transformer/texttransformers/LatexTextTransformerTest.java
+++ src/test/java/pl/put/poznan/transformer/texttransformers/LatexTextTransformerTest.java
@@ -3,7 +3,7 @@ package pl.put.poznan.transformer.texttransformers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LatexTextTransformerTest {
     private LatexTextTransformer transformer;
