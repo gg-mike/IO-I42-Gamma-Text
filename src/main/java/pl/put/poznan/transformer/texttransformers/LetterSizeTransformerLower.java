b07f0808ed5495876e250b1cb49665c13fc851e3
Michael Grygiel <47571348+gg-mike@users.noreply.github.com>
2021-12-20 08:05:36 +0100

    Update LetterSizeTransformerLower.java

--- src/main/java/pl/put/poznan/transformer/texttransformers/LetterSizeTransformerLower.java
+++ src/main/java/pl/put/poznan/transformer/texttransformers/LetterSizeTransformerLower.java
@@ -3,9 +3,14 @@ package pl.put.poznan.transformer.texttransformers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+/**
+ * Zmianać wielkości liter na lower
+ *
+ * @author Mikalai Chabatarevich
+ * @see TextTransformer
+ */
 @Slf4j
 @Component
-
 public class LetterSizeTransformerLower implements TextTransformer {
     public String transform(String text){
         return text.toLowerCase();
