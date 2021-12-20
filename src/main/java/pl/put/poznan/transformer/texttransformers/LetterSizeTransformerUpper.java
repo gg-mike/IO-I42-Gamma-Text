4d86c66bd9000739b99fb97406c1104714721136
Michael Grygiel <47571348+gg-mike@users.noreply.github.com>
2021-12-20 08:06:05 +0100

    Update LetterSizeTransformerUpper.java

--- src/main/java/pl/put/poznan/transformer/texttransformers/LetterSizeTransformerUpper.java
+++ src/main/java/pl/put/poznan/transformer/texttransformers/LetterSizeTransformerUpper.java
@@ -3,9 +3,14 @@ package pl.put.poznan.transformer.texttransformers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+/**
+ * Zmianać wielkości liter na upper
+ *
+ * @author Mikalai Chabatarevich
+ * @see TextTransformer
+ */
 @Slf4j
 @Component
-
 public class LetterSizeTransformerUpper implements TextTransformer {
     public String transform(String text){
         return text.toUpperCase();
