a9852c67ae848f8df5d0b01a069face32d0b43dd
KanarXD <piciolo@outlook.com>
2022-01-03 20:24:17 +0100

    add WordCountTextStatistic

--- src/main/java/pl/put/poznan/transformer/textstatistics/WordCountTextStatistic.java
+++ src/main/java/pl/put/poznan/transformer/textstatistics/WordCountTextStatistic.java
@@ -1,21 +1,19 @@
 package pl.put.poznan.transformer.textstatistics;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-
 /**
- * letter counter
+ * word counter
  * count the occurrences of all characters
  *
  * @author Piotr Parzysz
  */
 @Component
-public class LettersCountTextStatistic implements TextStatistic {
+public class WordCountTextStatistic implements TextStatistic {
 
     /**
      * text statistic method
@@ -25,9 +23,9 @@ public class LettersCountTextStatistic implements TextStatistic {
      */
     @Override
     public Object applyStatistic(String text) {
-        return Arrays.stream(text.split(""))
-                .map(String::toUpperCase)
-                .filter(letter -> !letter.isBlank())
+        return Arrays.stream(text.split(" "))
+                .map(String::toLowerCase)
+                .filter(word -> !word.isBlank())
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
