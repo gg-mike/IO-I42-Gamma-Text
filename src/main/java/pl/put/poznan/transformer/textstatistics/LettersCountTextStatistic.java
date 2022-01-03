62c34a93dbce8e495f6952c575ee87d09cebaf34
KanarXD <piciolo@outlook.com>
2022-01-03 18:35:51 +0100

    add LettersCountTextStatistic

---
+++ src/main/java/pl/put/poznan/transformer/textstatistics/LettersCountTextStatistic.java
@@ -0,0 +1,22 @@
+package pl.put.poznan.transformer.textstatistics;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class LettersCountTextStatistic implements TextStatistic {
+
+    @Override
+    public Object applyStatistic(String text) {
+        return Arrays.stream(text.split(""))
+                .map(String::toUpperCase)
+                .filter(letter -> !letter.equals(" "))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+}
