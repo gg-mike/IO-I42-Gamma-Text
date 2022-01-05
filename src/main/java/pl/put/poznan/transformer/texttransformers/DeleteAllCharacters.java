cf50e38799e1c5f278407b44df2bcd4cfcb04324
Николай Чеботаревич <kdiebro13012002@gmail.com>
2022-01-05 22:08:01 +0300

    +Usuwanie wszystkich znaków
    +Zamiana liczb na tekst

---
+++ src/main/java/pl/put/poznan/transformer/texttransformers/DeleteAllCharacters.java
@@ -0,0 +1,19 @@
+package pl.put.poznan.transformer.texttransformers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Deleting all characters
+ *
+ * @author Mikalai Chabatarevich
+ * @see TextTransformer
+ */
+@Slf4j
+@Component
+public class DeleteAllCharacters implements TextTransformer {
+    @Override
+    public String transform(String text){
+        return text.replaceAll("[^A-Za-z0-9' ']", "");
+    }
+}
